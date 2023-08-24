b8ae6cf66de8be24abd31f2ebf9e7c8eaa45948e
Muhammad Dafi <dafinoer@outlook.com>
2023-08-24 21:40:07 +0700

    add inject object and refactor

--- app/src/main/java/com/dafinrs/calculatorcompose/ui/pages/calculator/CalculatorScreenPage.kt
+++ app/src/main/java/com/dafinrs/calculatorcompose/ui/pages/calculator/CalculatorScreenPage.kt
@@ -9,30 +9,38 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.dafinrs.calculatorcompose.model.MathSymbol
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.dafinrs.calculatorcompose.data.repository.ArithmeticRepository
+import com.dafinrs.calculatorcompose.domains.model.MathSymbol
+import com.dafinrs.calculatorcompose.presentation.arithmetic.ArithmeticViewModel
 import com.dafinrs.calculatorcompose.presentation.typing.rememberEditTextState
-import com.dafinrs.calculatorcompose.repository.CalculatorRepository
 import com.dafinrs.calculatorcompose.ui.pages.calculator.components.ButtonsBox
 import com.dafinrs.calculatorcompose.ui.pages.calculator.components.TextViewBox
+import kotlinx.coroutines.Dispatchers
 import org.koin.compose.koinInject
+import org.koin.core.parameter.parametersOf
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorScreenPage() {
-    val calculatorRepository =
-        koinInject<CalculatorRepository>()
+    val arithmeticRepository =
+        koinInject<ArithmeticRepository>(parameters = { parametersOf(Dispatchers.Main) })
+    val arithmeticViewModel =
+        koinInject<ArithmeticViewModel>(parameters = { parametersOf(arithmeticRepository) })
 
     Scaffold {
         Column(modifier = Modifier.padding(it)) {
-            val editTextState = rememberEditTextState(initialState = "", calculatorRepository)
+            val editTextState = rememberEditTextState(initialState = "")
+            val arithmeticResult =
+                arithmeticViewModel.arithmeticUiState.collectAsStateWithLifecycle()
 
             TextViewBox(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.4f),
                 mathCharacter = editTextState.textState.value,
-                mathResult = editTextState.resultState.value
+                mathResult = arithmeticResult.value
             )
             ButtonsBox(
                 modifier = Modifier
@@ -69,19 +77,20 @@ fun CalculatorScreenPage() {
                     editTextState.addText("6")
                 },
                 onPressReduction = {
-                    editTextState.addSymbol(MathSymbol.KURANG)
+                    editTextState.addSymbol(MathSymbol.MINUS_SIGN)
                 },
                 onPressMultiplication = {
-                    editTextState.addSymbol(MathSymbol.KALI)
+                    editTextState.addSymbol(MathSymbol.TIMES_SIGN)
                 },
                 onPressPlusSign = {
-                    editTextState.addSymbol(MathSymbol.TAMBAH)
+                    editTextState.addSymbol(MathSymbol.PLUS_SIGN)
                 },
                 onPressDivisionSlash = {
-                    editTextState.addSymbol(MathSymbol.BAGI)
+                    editTextState.addSymbol(MathSymbol.DIVISION_SLASH)
                 },
                 onReset = {
                     editTextState.onReset()
+                    arithmeticViewModel.setToIdle()
                 },
                 onSetDouble = {
                     editTextState.onAddDouble()
@@ -93,7 +102,9 @@ fun CalculatorScreenPage() {
 
                 },
                 onPressResult = {
-                    editTextState.onResult()
+                    if (editTextState.isValidateCalculate()) {
+                        arithmeticViewModel.calculate(editTextState.textState.value)
+                    }
                 },
             )
         }
